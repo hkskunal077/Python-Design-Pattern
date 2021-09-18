a23e3b1e42c5937c2fe7ee8d87d5ad8dab3b24e1
Kunal Sharma <kushkunal077@gmail.com>
2021-09-18 18:47:25 +0530

    Update regexPattern.py

--- Iterator Pattern/regexPattern.py
+++ Iterator Pattern/regexPattern.py
@@ -1,20 +1,20 @@
 import re
 def match_regex(filename, regex):
-with open(filename) as file:
+  with open(filename) as file:
 lines = file.readlines()
 for line in reversed(lines):
-match = re.match(regex, line)
-if match:
-regex = yield match.groups()[0]
+  match = re.match(regex, line)
+  if match:
+    regex = yield match.groups()[0]
 def get_serials(filename):
 ERROR_RE = "XFS ERROR (\[sd[a-z]\])"
 matcher = match_regex(filename, ERROR_RE)
 device = next(matcher)
 while True:
-try:
-bus = matcher.send(
-"(sd \S+) {}.*".format(re.escape(device))
-)
+  try:
+  bus = matcher.send(
+  "(sd \S+) {}.*".format(re.escape(device))
+  )
 serial = matcher.send("{} \(SERIAL=([^)]*)\)".format(bus))
 yield serial
 device = matcher.send(ERROR_RE)
